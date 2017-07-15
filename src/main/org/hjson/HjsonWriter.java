5561bfdef5c66ce3ce4f9839614c057cd2eae6ef
Christian Zangl <laktak@cdak.net>
2017-07-15 22:22:20 +0200

    upd tests/check

--- src/main/org/hjson/HjsonWriter.java
+++ src/main/org/hjson/HjsonWriter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015-2016 Christian Zangl
+ * Copyright (c) 2015-2017 Christian Zangl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -30,7 +30,7 @@ class HjsonWriter {
 
   private IHjsonDsfProvider[] dsfProviders;
 
-  static Pattern needsEscapeName=Pattern.compile("[,\\{\\[\\}\\]\\s:#\"']|//|/\\*|'''");
+  static Pattern needsEscapeName=Pattern.compile("[,\\{\\[\\}\\]\\s:#\"']|//|/\\*");
 
   public HjsonWriter(HjsonOptions options) {
     if (options!=null) {
